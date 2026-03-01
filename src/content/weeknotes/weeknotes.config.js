9985629388f08b32b25826172d6bde958682ea22
Paul Smith <mail@paulsmith.site>
2026-03-01 21:49:56 +0000

    updated url structure and added redirects and legacy 11ty collection

--- src/content/weeknotes/weeknotes.config.js
+++ src/content/weeknotes/weeknotes.config.js
@@ -2,5 +2,8 @@ module.exports = {
   layout: 'weeknote.njk',
   feed: {
     url: '/weeknotes/feed.xml'
-  }
+  },
+  // Use a date + title-based URL for individual weeknotes
+  // e.g. /weeknotes/2021-04-05/weeknotes-1/
+  permalink: "weeknotes/{{ date | date('yyyy-LL-dd') }}/{{ title | slug }}/"
 }
